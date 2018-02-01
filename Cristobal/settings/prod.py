1101ae8f717143076397d482e7914bfa292414d7
Jonathan Basilio <jonathanbasilio24@outlook.com>
2018-02-01 01:03:45 -0600

    refactor: create url index

---
+++ Cristobal/settings/prod.py
@@ -0,0 +1,17 @@
+from pet_guru.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+ 
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PET_GURU_DB_NAME'),
+        'USER': os.getenv('PET_GURU_DB_USER'),
+        'PASSWORD': os.getenv('PET_GURU_DB_PASSWORD'),
+        'HOST': os.getenv('PET_GURU_DB_HOST'),
+        'PORT': os.getenv('PET_GURU_DB_PORT'),
+    }
+}
